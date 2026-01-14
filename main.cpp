1eec9058b9de94bc719620f922fd7273e4f240ff
BonBon125 <rongeorge12@icloud.com>
2026-01-14 19:26:36 +0000

    The video path is now input into the cpp program and outputed to the terminal.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+
+char* VideoPath;
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello" << std::endl;
+    VideoPath = argv[1];
+    std::cout << VideoPath << "\n";
+
+    // TODO: Open the Video Path and read the bytes to a buffer / new video object
+    // TODO: Convert the data in the video object / buffer to a gif object
+    // TODO: Write the contents of this gif object to a file
+
     return 0;
 }
